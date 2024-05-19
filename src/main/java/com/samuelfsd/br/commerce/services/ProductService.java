c32f608ddaaf9e13f84c9d0dbb76de9560291fa5
samuelfsd <samuel.estudo778@gmail.com>
2024-05-19 10:51:03 -0300

    feat: add product service

---
+++ src/main/java/com/samuelfsd/br/commerce/services/ProductService.java
@@ -0,0 +1,66 @@
+package com.samuelfsd.br.commerce.services;
+
+import com.samuelfsd.br.commerce.dtos.product.ProductRequestDTO;
+import com.samuelfsd.br.commerce.dtos.product.ProductResponseDTO;
+import com.samuelfsd.br.commerce.entities.Product;
+import com.samuelfsd.br.commerce.repositories.ProductRepository;
+import com.samuelfsd.br.commerce.util.ICRUDHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService implements ICRUDHandler<ProductRequestDTO, ProductResponseDTO> {
+
+    // TODO: complete all methods
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @Override
+    public List<ProductResponseDTO> getAll() {
+        return null;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public ProductResponseDTO getById(Long id) {
+        Optional<Product> productOpt = productRepository.findById(id);
+
+        if(productOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado um produto com este id");
+        }
+
+        Product product = productOpt.get();
+
+        ProductResponseDTO dto = new ProductResponseDTO();
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setDescription(product.getDescription());
+        dto.setPrice(product.getPrice());
+        dto.setImgUrl(dto.getImgUrl());
+
+        return dto;
+    }
+
+    @Override
+    public ProductResponseDTO create(ProductRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDTO update(Long id, ProductRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
